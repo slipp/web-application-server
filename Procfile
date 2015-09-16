6dce9980796a952f3a2cd2e6090059ec1f084191
javajigi <javajigi@gmail.com>
2015-09-16 10:51:16 +0900

    heroku의 port와 연결되도록 구현함.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -cp target/classes:target/dependency/* webserver.WebServer
+web:    java -cp target/classes:target/dependency/* webserver.WebServer $PORT
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -8,13 +8,20 @@ import org.slf4j.LoggerFactory;
 
 public class WebServer {
 	private static final Logger log = LoggerFactory.getLogger(WebServer.class);
-	private static final int DEFAULT_PORT = 80;
+	private static final int DEFAULT_PORT = 8080;
 	
-    public static void main(String argv[]) throws Exception {
+    public static void main(String args[]) throws Exception {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT; 
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+        
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
     	
-    	try (ServerSocket listenSocket = new ServerSocket(DEFAULT_PORT)) {
-    		log.info("Web Application Server started {} port.", DEFAULT_PORT);
+    	try (ServerSocket listenSocket = new ServerSocket(port)) {
+    		log.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
