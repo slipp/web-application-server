b2ea27a40f79b857e95f169b61f5426e6700fae4
javajigi <javajigi@gmail.com>
2015-12-15 16:30:18 +0900

    사용자 데이터를 관리하기 위한 DB 추가함.

---
+++ src/main/java/db/DataBase.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import model.User;
+
+public class DataBase {
+	private static Map<String, User> users = Maps.newHashMap();
+	
+	public static void addUser(User user) {
+		users.put(user.getUserId(), user);
+	}
+	
+	public static User findUserById(String userId) {
+		return users.get(userId);
+	}
+	
+	public static Collection<User> findAll() {
+		return users.values();
+	}
+}
