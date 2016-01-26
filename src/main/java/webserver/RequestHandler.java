30642bfc8b125f17da290a72c06ecd692fee30ad
javajigi <javajigi@gmail.com>
2016-01-26 13:36:08 +0900

    1단계 구현 완료함.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,14 +26,31 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 		
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			String[] tokens = line.split(" ");
+			while(!line.equals("")) {
+				log.debug("header : {}", line);
+				line = br.readLine();
+			}
+			
+			String url = getDefaultUrl(tokens);
+			
 			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello World".getBytes();
+			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 			response200Header(dos, body.length);
 			responseBody(dos, body);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private String getDefaultUrl(String[] tokens) {
+		String url = tokens[1];
+		if (url.equals("/")) {
+			url = "/index.html";
+		}
+		return url;
 	}
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
