be418e4dcae55a5783fd3a087696dea46c719663
javajigi <javajigi@gmail.com>
2016-01-26 16:39:29 +0900

    웹 서버 3번 문제까지 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,20 +35,43 @@ public class RequestHandler extends Thread {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
 			String[] tokens = line.split(" ");
+			
+			int contentLength = 0;
 			while(!line.equals("")) {
 				log.debug("header : {}", line);
 				line = br.readLine();
+				if (line.contains("Content-Length")) {
+					contentLength = getContentLength(line);
+				}
 			}
 			
 			String url = getDefaultUrl(tokens);
-			
-			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-			response200Header(dos, body.length);
-			responseBody(dos, body);
+			if ("/create".equals(url)) {
+				String body = IOUtils.readData(br, contentLength);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+				User user = new User(params.get("userId"), params.get("password"), 
+						params.get("name"), params.get("email"));
+				log.debug("user : {}", user);
+				DataBase.addUser(user);
+				responseResource(out, "/index.html");
+			} else {
+				responseResource(out, url);
+			}
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private void responseResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
+	}
+
+	private int getContentLength(String line) {
+		String[] headerTokens = line.split(":");
+		return Integer.parseInt(headerTokens[1].trim());
 	}
 
 	private String getDefaultUrl(String[] tokens) {
