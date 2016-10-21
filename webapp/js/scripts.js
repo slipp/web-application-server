13f48f539608617798d7d79b558e620ad7046d8b
javajigi <javajigi@gmail.com>
2016-10-21 11:10:24 +0900

    AJAX 구현을 위한 HTML로 수정함.

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,13 +1,9 @@
-
-$(document).ready(function(){/* jQuery toggle layout */
-$('#btnToggle').click(function(){
-  if ($(this).hasClass('on')) {
-    $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
-    $(this).removeClass('on');
-  }
-  else {
-    $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
-    $(this).addClass('on');
-  }
-});
-});
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+        ;
+  });
+};
