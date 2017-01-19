ce7116030ce9b2d3aaa5b7f87fa6745434f0cf5b
DG@stripes <leekoon@gmail.com>
2017-01-19 18:48:47 +0900

    첫 번째 요구사항에 대한 테스트 케이스 작성

--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -3,6 +3,7 @@ package util;
 import java.io.BufferedReader;
 import java.io.StringReader;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,5 +18,17 @@ public class IOUtilsTest {
         BufferedReader br = new BufferedReader(sr);
 
         logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+    }
+
+    @Test
+    public void getUrl() throws Exception {
+        String header_first_line = "GET /index.html GET HTTP/1.1";
+        String result = HttpRequestUtils.getUrl(header_first_line);
+        Assert.assertEquals(result, "/index.html");
+
+        header_first_line = "GET /test.html GET HTTP/1.1";
+        result = HttpRequestUtils.getUrl(header_first_line);
+        Assert.assertEquals(result, "/test.html");
+
     }
 }
