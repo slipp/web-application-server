3db4d6ebe12025edc8be264f8e18a8d223471159
qwebnm7788 <qwebnm7788@naver.com>
2018-10-18 15:52:38 +0900

    요구사항2 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -15,6 +15,9 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
+import util.HttpRequestUtils;
+
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -31,15 +34,37 @@ public class RequestHandler extends Thread {
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String[] requestHeader = br.readLine().split(" ");
-
+			
 			Map<String, String> header = new HashMap<>();
 			String line = null;
 			while (!(line = br.readLine()).equals("")) {
 				header.put(line.substring(0, line.indexOf(':')), line.substring(line.indexOf(':') + 1));
 			}
+			
+			String method = requestHeader[0];
+			String version = requestHeader[2];
+
+			int index = requestHeader[1].indexOf('?');
+			String URL = requestHeader[1];
+			String params = null;
+			
+			if(URL.equals("/")) URL = "/index.html";
+			
+			Map<String, String> queries = null;
+			if(index != -1) {
+				URL = requestHeader[1].substring(0, index);
+				params = requestHeader[1].substring(index + 1);
+				queries = HttpRequestUtils.parseQueryString(params);
+			}
+			
+			if(URL.startsWith("/user/create")) {
+				User user = new User(queries.get("userId"), queries.get("password"), queries.get("name"), queries.get("email"));
+				log.debug("user : {}", user);
+			}
+			
 			DataOutputStream dos = new DataOutputStream(out);
 
-			String filePath = "webapp" + requestHeader[1];
+			String filePath = "webapp" + URL;
 			log.info("filepath : {}", filePath);
 			byte[] body = Files.readAllBytes(Paths.get(filePath));
 
