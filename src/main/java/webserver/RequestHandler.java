d5bb34f4658a9d6cdaae7132afe66409dd4a2a1d
Hanbyul Kim <hahanbyul@gmail.com>
2019-03-23 23:39:09 +0900

    [FIX] get index.html

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +29,32 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	
+        	String url = "";
+        	String line;
+        	do {
+        		line = br.readLine();
+        		if (line == null) {
+        			return;
+        		}
+        		if (line.startsWith("GET")) {
+        			String[] tokens = line.split(" ");
+        			url = tokens[1];
+        			
+        			
+        		}
+        	}
+        	while (!"".equals(line));
+        	
+        	byte[] body;
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            if ("/".equals(url)) {
+            	body = "Hello World".getBytes();
+            } else {
+            	body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            }
+            
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
