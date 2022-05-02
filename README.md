5dbe5144ad20114b9a3a1e7a61669fb4aa39291e
RaeHyeonJeong <me1411111@gmail.com>
2022-05-02 19:10:28 +0900

    [UPDATE] 요구사항1 코드, Readme 정리

--- README.md
+++ README.md
@@ -15,7 +15,31 @@
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
-* 
+* **HTTP Request & Response 구조**
+  * **Request** 
+    * | Request Line ( 라인 구성: HTTP-메서드 URI HTTP-버전 )
+    * | Request Header
+    * | Request Header 와 Request Body 사이의 빈 공백 라인
+    * | Request Body
+    <br/><br/>
+  * **Response**
+    * | State Line ( 라인 구성: HTTP-버전 상태코드 응답구문 )
+    * | Response Header
+    * | Response Header 와 Request Body 사이의 빈 공백 라인
+    * | Response Body
+    <br/><br/>
+  * 각 Header와 Body 상세내용은 다를 수 있지만 전체적인 구조는 동일하게 구성 되어있다
+  <br/><br/>
+* **서버는 각 요청에 대하여 순차적으로 실행하는 것이 아님**
+* **동시에 각 요청에 대응하는 스레드를 생성해 동시 실행함.**
+  <br/><br/>
+* **서버는 웹 페이지를 구성하는 모든 자원을 한번에 응답으로 보내지 않는다!**
+  * 요청에 대한 처리 순서
+    1. /index.html 요청에 대한 응답에 HTML을 보냄
+    2. 응답 받은 브라우저는 HTML 내용을 분석해서 CSS, JAVASCRIPT, IMAGE 등 자원이 포함되어 있으면<br> 서버에 해당 자원을 다시 요청 
+  * 그러므로
+    * 하나의 웹페이지를 정상적으로 서비스 하려면 클라이언트와 서버 간 한번의 요청이 아닌<br>여러번의 요청과 응답을 주고 받게 됨.
+    
 
 ### 요구사항 2 - get 방식으로 회원가입
 * 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +22,24 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // inputStream BufferedReader 생성
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
+
+            if(line == null) {
+                return;
+            }
+
+            String[] tokens = line.split(" ");
+
+            while(!"".equals(line)){
+                line = br.readLine();
+                log.debug("header : {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
