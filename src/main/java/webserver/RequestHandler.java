c284c8389ead2fe3e3c7ef760bf9bf9dad3f1fa9
RaeHyeonJeong <me1411111@gmail.com>
2022-05-06 22:07:40 +0900

    [UPDATE] 요구사항3 코드 POST 방식 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -37,25 +38,32 @@ public class RequestHandler extends Thread {
             String[] tokens = line.split(" ");
             String url = tokens[1];
 
-            while(!line.equals("")){
-                line = br.readLine();
-                log.debug("header : {}", line);
-            }
-
-            // url이 회원가입 요청일 경우 처리 로직
+            // POST 방식 처리
             if (url.startsWith("/user/create")) {
-                int index = url.indexOf("?");
-                String queryString = url.substring(index + 1);
-                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                int contentLength = 0;
+
+                while(!line.equals("")){
+                    line = br.readLine();
+                    if(line.contains("Content-Length")){
+                        contentLength = getContentLength(line);
+                    }
+                    log.debug("header : {}", line);
+                }
+
+                String requestBody = IOUtils.readData(br, contentLength);
+                log.debug("body : {}", requestBody);
+
+                Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
 
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug(user.toString());
             } else {
-
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
                 response200Header(dos, body.length);
                 responseBody(dos, body);
             }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -79,5 +87,10 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(String line){
+        String[] headerTokens = line.split(" ");
+        return Integer.parseInt(headerTokens[1].trim());
     }
 }
