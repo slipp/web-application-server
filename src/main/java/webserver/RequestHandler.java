b4cb10daeb8752da371a675112d42411fd30a7bd
kmss6905 <kmss69052@naver.com>
2022-10-24 23:57:22 +0900

    요구사항 1단계 index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +23,33 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            // ??
+            InputStreamReader reader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String r;
+            byte[] bytes;
+
+            if ((r = bufferedReader.readLine()) != null) {
+                log.info(r);
+                String[] split = r.split(" ");
+                if (split[1].equals("/index.html")) {
+                    log.info("index.html 요청입니다.");
+
+                    // 파일읽기
+                    bytes = Files.readAllBytes(new File(("./webapp" + split[1])).toPath());
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response200Header(dos, bytes.length);
+                    responseBody(dos, bytes);
+                }else{
+                    // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+                    DataOutputStream dos = new DataOutputStream(out);
+                    byte[] body = "HelloWorld".getBytes();
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
