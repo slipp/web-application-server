afe9b259b1cba1ac4900da25c956160e20b41a37
JKjiwon <jwkim.oa@gmail.com>
2023-10-24 12:53:11 +0900

    feat: 3장_요구사항6 구현 - 사용자 목록 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,7 +10,9 @@ import util.IOUtils;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RequestHandler extends Thread {
@@ -38,18 +40,18 @@ public class RequestHandler extends Thread {
             String[] requestLine = line.split(" ");
 
             // request header
-            Map<String, String> headers = new HashMap<>();
+            Map<String, String> requestHeaders = new HashMap<>();
             while ((line = br.readLine()) != null && !line.equals("")) {
                 log.debug("[Request Header] {}", line);
                 String[] headerToken = line.split(": ");
-                headers.put(headerToken[0], headerToken[1]);
+                requestHeaders.put(headerToken[0], headerToken[1]);
             }
 
             String httpMethod = requestLine[0];
             String requestUrl = requestLine[1];
             if (httpMethod.equals("POST") && requestUrl.startsWith("/user/create")) {
                 // request body
-                String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                String requestBody = IOUtils.readData(br, Integer.parseInt(requestHeaders.get("Content-Length")));
                 log.debug("[Request Body] {}", requestBody);
 
                 Map<String, String> queryStringMap = HttpRequestUtils.parseQueryString(requestBody);
@@ -62,7 +64,7 @@ public class RequestHandler extends Thread {
                 DataBase.addUser(user);
                 redirect(dos, "/index.html");
             } else if (httpMethod.equals("POST") && requestUrl.equals("/user/login")) {
-                String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                String requestBody = IOUtils.readData(br, Integer.parseInt(requestHeaders.get("Content-Length")));
                 log.debug("[Request Body] {}", requestBody);
                 Map<String, String> queryStringMap = HttpRequestUtils.parseQueryString(requestBody);
                 String userId = queryStringMap.get("userId");
@@ -79,6 +81,15 @@ public class RequestHandler extends Thread {
                 // 로그인 성공
                 responseHeader.put("Set-Cookie", "logined=true; Path=/");
                 redirect(dos, "/index.html", responseHeader);
+            } else if (httpMethod.equals("GET") && requestUrl.equals("/user/list")) {
+                if (!isLogined(requestHeaders)) {
+                    redirect(dos, "/index.html");
+                    return;
+                }
+                List<User> users = new ArrayList<>(DataBase.findAll());
+                byte[] body = responseUsers(users);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             } else {
                 byte[] body = Files.readAllBytes(new File(HTML_FILE_PATH + requestLine[1]).toPath());
                 response200Header(dos, body.length);
@@ -87,6 +98,43 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private byte[] responseUsers(List<User> users) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table>");
+        sb.append("<tr>");
+        sb.append("<th>userId</th>");
+        sb.append("<th>name</th>");
+        sb.append("<th>email</th>");
+        sb.append("</tr>");
+
+        for (User user : users) {
+            sb.append("<tr>");
+            sb.append("<td>");
+            sb.append(user.getUserId());
+            sb.append("</td>");
+            sb.append("<td>");
+            sb.append(user.getName());
+            sb.append("</td>");
+            sb.append("<td>");
+            sb.append(user.getEmail());
+            sb.append("</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        return sb.toString().getBytes();
+    }
+
+    private boolean isLogined(Map<String, String> requestHeaders) {
+        String cookies = requestHeaders.get("Cookie");
+        Map<String, String> cookieMap = HttpRequestUtils.parseCookies(cookies);
+        String logined = cookieMap.get("logined");
+        if (logined == null) {
+            return false;
+        }
+
+        return Boolean.parseBoolean(logined);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
