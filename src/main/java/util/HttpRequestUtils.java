35c969c4ba16f2cc7404cbbdd5e4a81178f0b5a7
JKjiwon <jwkim.oa@gmail.com>
2023-10-24 09:37:36 +0900

    주석 수정

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -9,8 +9,8 @@ import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
     /**
-     * @param queryString은
-     *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+     * @param queryString
+     * URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
      */
     public static Map<String, String> parseQueryString(String queryString) {
@@ -18,7 +18,7 @@ public class HttpRequestUtils {
     }
 
     /**
-     * @param 쿠키
+     * @param cookies
      *            값은 name1=value1; name2=value2 형식임
      * @return
      */
--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 
 public class IOUtils {
     /**
-     * @param BufferedReader는
-     *            Request Body를 시작하는 시점이어야
-     * @param contentLength는
-     *            Request Header의 Content-Length 값이다.
+     * @param br            는
+     *                      Request Body를 시작하는 시점이어야
+     * @param contentLength 는
+     *                      Request Header의 Content-Length 값이다.
      * @return
      * @throws IOException
      */
