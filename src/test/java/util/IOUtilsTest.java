fc4e2953632ee1677353a4e78c850036c403f8d5
JKjiwon <jwkim.oa@gmail.com>
2023-10-24 10:16:49 +0900

    refactor: Test 프레임워크를 junit5, assertJ로 변경

--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -1,11 +1,11 @@
 package util;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
