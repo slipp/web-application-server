0595de59d99bd53a7ee9f85492900e438f2bc532
Shin Changyu <scv1702@naver.com>
2024-02-26 20:56:17 +0900

    chore: Maven에서 Gradle로 이관 및 Junit 버전 업

--- src/test/java/util/HttpRequestUtilsTest.java
+++ src/test/java/util/HttpRequestUtilsTest.java
@@ -1,12 +1,10 @@
 package util;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Map;
 
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import util.HttpRequestUtils.Pair;
 
 public class HttpRequestUtilsTest {
@@ -14,60 +12,60 @@ public class HttpRequestUtilsTest {
     public void parseQueryString() {
         String queryString = "userId=javajigi";
         Map<String, String> parameters = HttpRequestUtils.parseQueryString(queryString);
-        assertThat(parameters.get("userId"), is("javajigi"));
-        assertThat(parameters.get("password"), is(nullValue()));
+        assertThat(parameters.get("userId")).isEqualTo("javajigi");
+        assertThat(parameters.get("password")).isNull();
 
         queryString = "userId=javajigi&password=password2";
         parameters = HttpRequestUtils.parseQueryString(queryString);
-        assertThat(parameters.get("userId"), is("javajigi"));
-        assertThat(parameters.get("password"), is("password2"));
+        assertThat(parameters.get("userId")).isEqualTo("javajigi");
+        assertThat(parameters.get("password")).isEqualTo("password2");
     }
 
     @Test
     public void parseQueryString_null() {
         Map<String, String> parameters = HttpRequestUtils.parseQueryString(null);
-        assertThat(parameters.isEmpty(), is(true));
+        assertThat(parameters.isEmpty()).isTrue();
 
         parameters = HttpRequestUtils.parseQueryString("");
-        assertThat(parameters.isEmpty(), is(true));
+        assertThat(parameters.isEmpty()).isTrue();
 
         parameters = HttpRequestUtils.parseQueryString(" ");
-        assertThat(parameters.isEmpty(), is(true));
+        assertThat(parameters.isEmpty()).isTrue();
     }
 
     @Test
     public void parseQueryString_invalid() {
         String queryString = "userId=javajigi&password";
         Map<String, String> parameters = HttpRequestUtils.parseQueryString(queryString);
-        assertThat(parameters.get("userId"), is("javajigi"));
-        assertThat(parameters.get("password"), is(nullValue()));
+        assertThat(parameters.get("userId")).isEqualTo("javajigi");
+        assertThat(parameters.get("password")).isNull();
     }
 
     @Test
     public void parseCookies() {
         String cookies = "logined=true; JSessionId=1234";
         Map<String, String> parameters = HttpRequestUtils.parseCookies(cookies);
-        assertThat(parameters.get("logined"), is("true"));
-        assertThat(parameters.get("JSessionId"), is("1234"));
-        assertThat(parameters.get("session"), is(nullValue()));
+        assertThat(parameters.get("logined")).isEqualTo("true");
+        assertThat(parameters.get("JSessionId")).isEqualTo("1234");
+        assertThat(parameters.get("session")).isNull();
     }
 
     @Test
-    public void getKeyValue() throws Exception {
+    public void getKeyValue() {
         Pair pair = HttpRequestUtils.getKeyValue("userId=javajigi", "=");
-        assertThat(pair, is(new Pair("userId", "javajigi")));
+        assertThat(pair).isEqualTo(new Pair("userId", "javajigi"));
     }
 
     @Test
-    public void getKeyValue_invalid() throws Exception {
+    public void getKeyValue_invalid() {
         Pair pair = HttpRequestUtils.getKeyValue("userId", "=");
-        assertThat(pair, is(nullValue()));
+        assertThat(pair).isNull();
     }
 
     @Test
-    public void parseHeader() throws Exception {
+    public void parseHeader() {
         String header = "Content-Length: 59";
         Pair pair = HttpRequestUtils.parseHeader(header);
-        assertThat(pair, is(new Pair("Content-Length", "59")));
+        assertThat(pair).isEqualTo(new Pair("Content-Length", "59"));
     }
 }
--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -3,7 +3,7 @@ package util;
 import java.io.BufferedReader;
 import java.io.StringReader;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
