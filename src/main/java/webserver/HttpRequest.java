b23a02005aa190d81b374863ec771c8cecbfa658
Shin Changyu <scv1702@naver.com>
2024-02-28 19:28:49 +0900

    feat: Http 요청에 대한 HttpRequest 클래스 구현

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,68 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import util.HttpRequestUtils;
+import util.HttpRequestUtils.Pair;
+
+enum HttpMethod {
+    GET, POST, PUT, DELETE
+}
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+    private String uri;
+    private HttpMethod method;
+    private Map<String, String> headers = new HashMap<>();
+
+    private HttpRequest(InputStream in) throws IOException {
+        parseHttpRequest(in);
+    }
+
+    public static HttpRequest from(InputStream in) throws IOException {
+        return new HttpRequest(in);
+    }
+
+    private void parseRequestLine(String line) {
+        String[] splited = line.split(" ");
+        System.out.println(Arrays.toString(splited));
+        method = HttpMethod.valueOf(splited[0]);
+        uri = splited[1];
+    }
+
+    private void parseHeaders(List<String> headers) {
+        headers.forEach(header -> {
+            Pair pair = HttpRequestUtils.parseHeader(header);
+            this.headers.put(pair.getKey(), pair.getKey());
+        });
+    }
+
+    private void parseHttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        parseRequestLine(br.readLine());
+        List<String> headers = new ArrayList<>();
+        for (String line = br.readLine(); !"".equals(line) && line != null; line = br.readLine()) {
+            headers.add(line);
+        }
+        parseHeaders(headers);
+    }
+
+    public HttpMethod getMethod() {
+        return this.method;
+    }
+
+    public String getUri() {
+        return this.uri;
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,56 +1,24 @@
 package webserver;
 
-import java.util.*;
-
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import util.HttpRequestUtils;
-import util.HttpRequestUtils.Pair;
-
-enum HttpMethod {
-    GET, POST, PUT, DELETE
-}
 
 public class RequestHandler extends Thread {
 
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
-    private String uri;
-    private HttpMethod method;
-    private Map<String, String> httpHeaders = new HashMap<>();
+    
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-    }
-
-    public void parseHttpHeaders(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String header = br.readLine();
-        
-        String[] splited = header.split(" ");
-        System.out.println(Arrays.toString(splited));
-        method = HttpMethod.valueOf(splited[0]);
-        uri = splited[1];
-
-        header = br.readLine();
-        while (header != null && !"".equals(header)) {
-            Pair pair = HttpRequestUtils.parseHeader(header);
-            httpHeaders.put(pair.getKey(), pair.getKey());
-            header = br.readLine();
-        }
     }
 
     public void run() {
@@ -58,8 +26,9 @@ public class RequestHandler extends Thread {
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "404 NOT FOUND".getBytes();
-            parseHttpHeaders(in);
+            HttpRequest httpRequest = HttpRequest.from(in);
+            String uri = httpRequest.getUri();
+            byte[] body = Files.readAllBytes(Paths.get("./webapp" + uri));
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
