0b01541abc12cc4c4f4d021da086525f03a506d7
Shin Changyu <scv1702@naver.com>
2024-02-28 18:54:08 +0900

    chore: 기본 포트를 58080으로 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_PORT = 58080;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,7 +18,7 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+        // 서버소켓을 생성한다. 웹서버는 기본적으로 58080번 포트를 사용한다.
 
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
