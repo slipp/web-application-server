2b193a4e82ba47adc470719806574ff0cbd54cf1
Shin Changyu <scv1702@naver.com>
2024-02-28 18:57:21 +0900

    feat: Http Header 파싱 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,31 +1,65 @@
 package webserver;
 
+import java.util.*;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RequestHandler extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+import util.HttpRequestUtils;
+import util.HttpRequestUtils.Pair;
 
+enum HttpMethod {
+    GET, POST, PUT, DELETE
+}
+
+public class RequestHandler extends Thread {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
+    private String uri;
+    private HttpMethod method;
+    private Map<String, String> httpHeaders = new HashMap<>();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
+    public void parseHttpHeaders(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String header = br.readLine();
+        
+        String[] splited = header.split(" ");
+        System.out.println(Arrays.toString(splited));
+        method = HttpMethod.valueOf(splited[0]);
+        uri = splited[1];
+
+        header = br.readLine();
+        while (header != null && !"".equals(header)) {
+            Pair pair = HttpRequestUtils.parseHeader(header);
+            httpHeaders.put(pair.getKey(), pair.getKey());
+            header = br.readLine();
+        }
+    }
+
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = "404 NOT FOUND".getBytes();
+            parseHttpHeaders(in);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
